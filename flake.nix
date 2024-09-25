2cf85676efea5dd77b8a3c7f82a0320b9f5de08a
hyperbarq <kubawlas1@gmail.com>
2024-09-25 02:45:17 +0200

    a

--- flake.nix
+++ flake.nix
@@ -14,29 +14,30 @@
   outputs = { self, nixpkgs, ... }@inputs:
   {
     diskoConfigurations = {
-            name = "vm";
-            value.disko.devices = {
-                main = {
-                    device = "/dev/sda";
-                    type = "disk";
-                    content = {
-                        type = "gpt";
-                        partitions = {
-                            ESP = {
-                                type = "EF00";
-                                size = "1G";
-                                content = {
-                                    type = "filesystem";
-                                    format = "vfat";
-                                    mountpoint = "/boot";
+            vm.disko.devices = {
+                disk = {
+                    main = {
+                        device = "/dev/sda";
+                        type = "disk";
+                        content = {
+                            type = "gpt";
+                            partitions = {
+                                ESP = {
+                                    type = "EF00";
+                                    size = "1G";
+                                    content = {
+                                        type = "filesystem";
+                                        format = "vfat";
+                                        mountpoint = "/boot";
+                                    };
                                 };
-                            };
-                            root = {
-                                size = "100%";
-                                content = {
-                                    type = "filesystem";
-                                    format = "ext4";
-                                    mountpoint = "/";
+                                root = {
+                                    size = "100%";
+                                    content = {
+                                        type = "filesystem";
+                                        format = "ext4";
+                                        mountpoint = "/";
+                                    };
                                 };
                             };
                         };
