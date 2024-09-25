3a0161034e13e09236f30974e66d8239fc0f1c41
hyperbarq <kubawlas1@gmail.com>
2024-09-25 02:15:00 +0200

    a

--- flake.nix
+++ flake.nix
@@ -1,1 +1,48 @@
- 
+{
+  description = "NixOS config flake";
+
+  inputs = {
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+  {
+    diskoConfigurations = {
+        vm = {
+            main = {
+                device = "/dev/sda";
+                type = "disk";
+                content = {
+                    type = "gpt";
+                    partitions = {
+                        ESP = {
+                            type = "EF00";
+                            size = "1G";
+                            content = {
+                                type = "filesystem";
+                                format = "vfat";
+                                mountpoint = "/boot";
+                            };
+                        };
+                        root = {
+                            size = "100%";
+                            content = {
+                                type = "filesystem";
+                                format = "ext4";
+                                mountpoint = "/";
+                            };
+                        };
+                    };
+                };
+            };
+        };
+    };
+  };
+}
+
