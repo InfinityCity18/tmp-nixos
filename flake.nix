7ccb4f755916b48956afaf6685b153a4b116ac31
hyperbarq <kubawlas1@gmail.com>
2024-09-25 02:20:44 +0200

    a

--- flake.nix
+++ flake.nix
@@ -15,27 +15,29 @@
   {
     diskoConfigurations = {
         vm = {
-            main = {
-                device = "/dev/sda";
-                type = "disk";
-                content = {
-                    type = "gpt";
-                    partitions = {
-                        ESP = {
-                            type = "EF00";
-                            size = "1G";
-                            content = {
-                                type = "filesystem";
-                                format = "vfat";
-                                mountpoint = "/boot";
+            disko.devices = {
+                main = {
+                    device = "/dev/sda";
+                    type = "disk";
+                    content = {
+                        type = "gpt";
+                        partitions = {
+                            ESP = {
+                                type = "EF00";
+                                size = "1G";
+                                content = {
+                                    type = "filesystem";
+                                    format = "vfat";
+                                    mountpoint = "/boot";
+                                };
                             };
-                        };
-                        root = {
-                            size = "100%";
-                            content = {
-                                type = "filesystem";
-                                format = "ext4";
-                                mountpoint = "/";
+                            root = {
+                                size = "100%";
+                                content = {
+                                    type = "filesystem";
+                                    format = "ext4";
+                                    mountpoint = "/";
+                                };
                             };
                         };
                     };
